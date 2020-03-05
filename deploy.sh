5c4ca25374394354dc77425ee74565b41b3d3058
Arn0 <jsuseone@gmail.com>
2020-03-05 18:58:58 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd stage/_book
+
+git init
+git config user.name "i@arn0.org"
+git config user.email "i@arn0.org"
+
+git remote add upstream "git@github.com:arn0blog/arn0page.git"
+git fetch upstream
+git reset upstream/master
+
+echo "arn0.org" > CNAME
+
+touch .
+
+# Push To Github
+#git add *
+#git commit -m "update"
+#git push origin master -f
+
+# Push To Coding
+git remote set-url origin https://CO_TOKEN@e.coding.net/arn0/MainPage.git
+git add *
+git commit -m "update"
+git push origin master -f
