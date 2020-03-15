3f91fdfbc7f4154937a8e44c33c7efd4df6f4d02
Arn0 <jsuseone@gmail.com>
2020-03-16 00:22:35 +0800

    Fast Push

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,6 @@ git remote add upstream https://github.com/arn0blog/arn0page.git
 git fetch upstream
 git reset upstream/test
 
-cp src/index.html /tmp
-git checkout master
-mv /tmp/index.html .
 echo "arn0.org" > CNAME
 git add index.html CNAME
 git commit -m "update'
